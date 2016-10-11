1ffbc18cd17b57a423697c062ed852fbbf81cf51
LevyCen <levy_02208@hotmail.com>
2016-10-11 17:41:03 -0500

    Lee todos los CSV de una carpeta

--- minMax.py
+++ minMax.py
@@ -1,17 +1,66 @@
+from string import *
+from os import listdir
 
-#Abrimos un archivo csv
-file = open("fichero_salida_2015918.csv","r")
+#Variables
+mini = 100
+maximo = 0
+total = 0
+dic = {} #diccionario vacio
 
-linea = file.readline()
-contador = 1
+#Se busca los archivos csv para trabajar
+for fileCSV in listdir("."):
+    ext = split(fileCSV,'.')
+    if not ext[1]=="csv":
+        continue
 
-for linea in file.readlines():
-    print linea
-    contador = contador + 1
+    dic.clear()
 
-print contador
+    f = open("fichero_salida_2015938.csv")
+    #f = open(fileCSV)
 
+    for lineaDeCSV in f:
+        tok = split(lineaDeCSV,',')
+        #print tok
+        minuto = strip(tok[10])
+        if not dic.has_key(tok[4]):
+            dic.update( {tok[4]: {minuto: 0}})  # diccionario con AP como clave
+        else:
+            if not dic[tok[4]].has_key(minuto):
+                dic[tok[4]].update( {minuto: 0} )
+        dic[tok[4]][minuto] = int(dic[tok[4]][minuto]) + 1
 
-#Cerramos el archivo
-file.close()
+        #print dic[tok[4]][strip(tok[10])]
+
+    #print dic
+    print ("Total de AP: ",len(dic))
+
+    for AP in dic:
+        #print AP
+        for nDev in dic[AP].values():
+            #print nDev
+            if nDev<mini:
+                mini=nDev
+            if nDev>maximo:
+                maximo=nDev
+            total=total+nDev
+
+        minutosRegistrados = len(dic[AP].values())
+        minutosRegCompletos = 30
+        if minutosRegistrados < minutosRegCompletos:
+            mini = 0
+
+        """
+        print(AP)
+        print ("Minimo de dispositivos: ",mini)
+        print ("Maximo de dispositivos: ",maximo)
+        print ("Total en la hr: ",total)
+        print ("total minutos registrados: ", len(dic[AP].values()) )
+        """
+        stringResultados = '' + str(AP) +','+ str(mini) +','+ str(maximo) +','+ str(total) +','+ str(len(dic[AP].values()))
+        print stringResultados
+
+        mini=100
+        maximo=0
+        total=0
+
 
